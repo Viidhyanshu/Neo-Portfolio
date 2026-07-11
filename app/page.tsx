2462477417466908b3f38723c7d20fcc959d71b5
Vidhyanshu Kumar <vidhyanshukumarvidhyam@gmail.com>
2026-07-11 09:35:52 +0530

    added skill section

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Projects from "@/components/Projects";
 import GitHubContributions from "@/components/contributions";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
@@ -55,6 +56,9 @@ export default function Home() {
 
       {/* GitHub Contributions Section */}
       <GitHubContributions />
+
+      {/* Skills Section */}
+      <Skills />
     </div>
   );
 }
